750c4a1be72d44bd067bb527a8f2fdeae6243ae0
Taryar Linn Phyar <35889976+taryarlin@users.noreply.github.com>
2023-10-17 16:34:37 +0630

    Fixed minor issue

--- app/Http/Resources/BlogResource.php
+++ app/Http/Resources/BlogResource.php
@@ -28,7 +28,7 @@ class BlogResource extends JsonResource
         return [
             'id' => $this->id,
             'title' => $this->title,
-            'body' => isset($this->auth_user) ? $this->body : Str::limit($this->body, 30),
+            'body' => $this->auth_user ? $this->body : Str::limit($this->body, 30),
             'user' => new UserResource($this->User),
             'categories' => CategoryResource::collection($this->Category),
             'created_at' => Carbon::parse($this->created_at)->format('Y-m-d H:i:s'),
